96ac99b8bcfd0d3af204a5473dce93af0d48a41f
meghana <maggi.kolapalli@gmail.com>
2023-09-17 20:25:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function UserProfile() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserProfiles() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const data = response.data;
+        setUsers(data);
+      } catch (error) {
+        console.log('Error fetching user profiles:', error);
+      }
+    }
+     fetchUserProfiles();
+  }, []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Profiles</h1>
+      {users.map((user) => (
+        <div key={user.name}>
+          <img
+            src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+            alt="Avatar"
+            style={{ width: '100px', height: '100px' }}
+          />
+          <h2>{user.name}</h2>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>
+            Address: {`${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}`}
+          </p>
+          <p>Website: {user.website}</p>
+          <p>Company: {user.company.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default UserProfile;
